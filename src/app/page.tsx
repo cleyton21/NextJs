badab5d8a6bf9c03b787a304143bdb6d235c4a83
root <root@DGO-SEO19.sef2.eb.mil.br>
2024-12-24 10:48:10 -0300

    Componente Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import Link from "next/link"
 
 export default function Home() {
   return (
+    <>
      <h1>Hello</h1>
+     <Link href={'/products'}>Ir para a página de produtos</Link>
+    </>
+
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,18 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: 'Produtos',
 }
 
 export default function Products() {
+
+    const id = 1;
     return (
         <div>
-            <h1>Products</h1>
+            <Link href={`/products/${id}`}>produto 1</Link>
+            <Link href={'/products/2'}>produto 2</Link>
+            <Link href={'/products/3'}>produto 3</Link>
         </div>
     );
 }
